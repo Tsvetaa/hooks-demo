b40f1ef66bf167abbd91b6400499dc86c3054a92
petkots <tsveta.v.petkova@gmail.com>
2023-04-10 16:25:55 +0200

    isCompleted

--- src/App.js
+++ src/App.js
@@ -48,8 +48,23 @@ function App() {
     setTodos(state => state.filter(x => x._id !== todoId));
   }
 
+  const onTodoClick = async(todoId) => {
+    const todo = todos.find(x => x._id === todoId)
+    
+    await fetch(`${baseUrl}/${todoId}`, {
+      method: 'PUT',
+      headers: {
+        'content-type': 'application/json'
+      },
+      body: JSON.stringify({...todo, isCompleted: !todo.isCompleted}) 
+    });
+
+    setTodos(state => state.map(x => x._id === todoId ? {...x, isCompleted: !x.isCompleted} : x));
+  };
+
   const contextValue = {
-    onTodoDeleteClick
+    onTodoDeleteClick,
+    onTodoClick,
   }
 
   return (
